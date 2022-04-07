85ac3efa74bf2af0bd50da29671fbed984e5dfba
Mohan <86612173+BW17-Arthur@users.noreply.github.com>
2022-04-07 18:04:04 +0800

    Update index.js

--- 落地页01/js/index.js
+++ 落地页01/js/index.js
@@ -1,28 +1,31 @@
 var link = {
     //亚博链接 
-    yaboHomePc: 'https://www.1ch2x.com:7443/register/?i_code='+yaboCode,
-    yaboPcHome: 'https://www.1ch2x.com:7443/app/home/?i_code='+yaboCode,
-    yaboHomeH5: 'https://www.ncggik.com:6001/entry/register/?i_code='+yaboCode,
-    yaboHome: 'https://www.ncggik.com:6001/app/home/?i_code='+yaboCode,
-    yaboApp: 'https://www.btfxec.com:6001/?i_code='+yaboCode,
+    yaboPc: 'https://www.1651yabo.com:9024/register/?i_code='+yaboCode,
+    yaboPcHome: 'https://www.1651yabo.com:9024/app/home/?i_code='+yaboCode,
+    yaboH5: 'https://www.jovjyj.com:9003/entry/register/?i_code='+yaboCode,
+    yaboH5Home: 'https://www.jovjyj.com:9003/app/home/?i_code='+yaboCode,
+    yaboApp: 'https://www.yabet106.app:9109/?i_code='+yaboCode,
+    
    
     // HTH链接
-    hthHomePc: 'https://www.8xorqeft.com:9966/register/?i_code='+hthCode,
+    hthPc: 'https://www.8xorqeft.com:9966/register/?i_code='+hthCode,
     hthPcHome: 'https://www.8xorqeft.com:9966/app/home/?i_code='+hthCode,
-    hthHomeH5: 'https://www.8xorqeft.com:9966/entry/register/?i_code='+hthCode,
-    hthHome: 'https://www.8xorqeft.com:9966/app/home/?i_code='+hthCode,
-    hthApp: 'https://www.vgdqb.com:6443/?i_code='+hthCode,
-  
+    hthH5: 'https://www.hthvip96.com:9964/entry/register/?i_code='+hthCode,
+    hthH5Home: 'https://www.hthvip96.com:9964/app/home/?i_code='+hthCode,
+    hthApp: 'https://www.xxxdwf.com:8005/?i_code='+hthCode,
+    
+    
+
     // 美洽客服： 妮妮
     contact: 'https://serv003.hdchat.cc',
     // 美洽客服： 诗兰
     contact1: 'https://serv08.hdchat.cc',
     // 美洽客服： 雅琳
     contact2: 'https://serv03.hdchat.cc',
+    // AChat_9组
+    contact3: 'https://chatweb.0jt082.com/chat/text/chat_1pIAoF.html',
     
    
-    
-                               
 }
 
 
@@ -41,16 +44,73 @@ function getDevice() {
 const device = getDevice();
 
 function register(key) {
-    if (key === 'yaboHome') {
-        window.location.href = device == 'pc' ? link.yaboHomePc : link.yaboHomeH5;
-    }else if (key === 'hthHome') {
-        window.location.href = device == 'pc' ? link.hthHomePc : link.hthHomeH5;
+    if (key === 'yabo') {
+        window.location.href = device == 'pc' ? link.yaboPc : link.yaboH5;
+    }else if (key === 'hth') {
+        window.location.href = device == 'pc' ? link.hthPc : link.hthH5;
     }else if (key === 'yaboHome') {
-        window.location.href = device == 'pc' ? link.yaboPcHome : link.yaboHome;
+        if(/(UCBrowser|UC|Quark)/i.test(navigator.userAgent)){
+            // alert(navigator.userAgent);
+            window.location.href = device == 'pc' ? link.yaboPcHome : link.yaboH5;
+        }else{
+            // alert("TEST "+navigator.userAgent);
+            // window.location.href = device == 'pc' ? link.yaboPcHome : link.yaboH5;
+            window.location.href = device == 'pc' ? link.yaboPcHome : link.yaboH5Home;
+        }
+        
+        
     }else if (key === 'hthHome') {
-        window.location.href = device == 'pc' ? link.hthPcHome : link.hthHome;
+        if(/(UCBrowser|UC|Quark)/i.test(navigator.userAgent)){
+            // alert(navigator.userAgent);
+            window.location.href = device == 'pc' ? link.hthPcHome : link.hthH5;
+        }else{
+            // alert("TEST "+navigator.userAgent);
+            // window.location.href = device == 'pc' ? link.hthPcHome : link.hthH5;
+            window.location.href = device == 'pc' ? link.hthPcHome : link.hthH5Home; 
+        }
+        
     }
     else {
         window.location.href = link[key]
     }
 }
+
+function registerOpen(key) {
+    if (key === 'yabo') {
+        if (device == 'pc'){
+            window.open(`${link.yaboPc}`);
+        }else{
+            window.open(`${link.yaboH5}`);
+        }
+        // window.location.href = device == 'pc' ? link.yaboPc : link.yaboH5;
+    }else if (key === 'hth') {
+        if (device == 'pc'){
+            window.open(`${link.hthPc}`);
+        }else{
+            window.open(`${link.hthH5}`);
+        }
+        // window.location.href = device == 'pc' ? link.hthPc : link.hthH5;
+    }else if (key === 'yaboHome') {
+        if (device == 'pc'){
+            window.open(`${link.yaboPcHome}`);
+        }else{
+            window.open(`${link.yaboH5Home}`);
+        }
+        // window.location.href = device == 'pc' ? link.yaboPcHome : link.yaboH5Home;
+    }else if (key === 'hthHome') {
+        if (device == 'pc'){
+            window.open(`${link.hthPcHome}`);
+        }else{
+            window.open(`${link.hthH5Home}`);
+        }
+        // window.location.href = device == 'pc' ? link.hthPcHome : link.hthH5Home;
+    }
+    else {
+        window.open(`${link[key]}`);
+        // window.location.href = link[key]
+    }
+}
+
+
+
+
